5bc0ebc7da8df27313443a3a5b5dc98cb27aedbc
Crowdbotics <team@crowdbotics.com>
2021-08-14 22:59:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "tight_waterfall_29819Identifier",
+  url: "https://tight_waterfall_29819Identifier.botics.co",
+  api: "https://tight_waterfall_29819Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
